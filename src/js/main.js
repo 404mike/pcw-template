38a18dc5562f4c15f41ab94cf887ed2adb93058e
Mike Jones <michaeljones401@gmail.com>
2024-05-20 11:55:01 +0100

    maps

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 import { initFacets } from '../js/leftsidebar/facets.js';
+import { initMap } from '../js/maps.js';
 
 
 initFacets();
