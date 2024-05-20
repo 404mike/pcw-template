69e4eae1d06314777013b811a7fde6eb0ea5b198
Mike Jones <michaeljones401@gmail.com>
2024-05-20 12:07:40 +0100

    maps

--- src/js/maps.js
+++ src/js/maps.js
@@ -1,33 +1,60 @@
-// AIzaSyD2IjeESJzSXKrTzkHZVsxtkVTgXBSMW8M
-import { Loader } from "@googlemaps/js-api-loader"
+// // AIzaSyD2IjeESJzSXKrTzkHZVsxtkVTgXBSMW8M
+// import { Loader } from "@googlemaps/js-api-loader"
 
+
+// // function initMap() {
+// //     // Load the JSON style from a local file
+// //     fetch('gmap-style.json')
+// //     .then(function(response) { return response.json(); })
+// //     .then(function(styles) {
+// //         // Create a map object centered on Wales
+// //         const wales = {lat: 52.1307, lng: -3.7837};  // Central coordinates for Wales
+// //         const mapOptions = {
+// //             zoom: 8,
+// //             center: wales,
+// //             styles: styles
+// //         };
+
+// //         // Instantiate the map
+// //         const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+// //     })
+// //     .catch(error => console.error('Error loading the style: ', error));
+// // }
 
 // function initMap() {
-//     // Load the JSON style from a local file
-//     fetch('gmap-style.json')
-//     .then(function(response) { return response.json(); })
-//     .then(function(styles) {
-//         // Create a map object centered on Wales
-//         const wales = {lat: 52.1307, lng: -3.7837};  // Central coordinates for Wales
-//         const mapOptions = {
-//             zoom: 8,
-//             center: wales,
-//             styles: styles
-//         };
+//     const loader = new Loader({
+//         apiKey: "AIzaSyD2IjeESJzSXKrTzkHZVsxtkVTgXBSMW8M",
+//         version: "weekly",
+//       });
+      
+//       loader.importLibrary().then(async () => {
+//         const { Map } = await google.maps.importLibrary("maps");
+//         console.log("Map");
+      
+//         map = new Map(document.getElementById("map"), {
+//           center: { lat: -34.397, lng: 150.644 },
+//           zoom: 8,
+//         });
+//       });
 
-//         // Instantiate the map
-//         const map = new google.maps.Map(document.getElementById('map'), mapOptions);
-//     })
-//     .catch(error => console.error('Error loading the style: ', error));
+//       console.log('initMap');
 // }
 
-function initMap() {
-    const loader = new Loader({
-        apiKey: "AIzaSyD2IjeESJzSXKrTzkHZVsxtkVTgXBSMW8M",
-        version: "weekly",
-      });
-      
-      loader.load().then(async () => {
+
+
+
+import { Loader } from "@googlemaps/js-api-loader";
+let map;
+const additionalOptions = {};
+// [START maps_programmatic_load_promise]
+const loader = new Loader({
+  apiKey: "AIzaSyD2IjeESJzSXKrTzkHZVsxtkVTgXBSMW8M",
+  version: "weekly",
+  ...additionalOptions,
+});
+
+function initMap(){
+    loader.load().then(async () => {
         const { Map } = await google.maps.importLibrary("maps");
       
         map = new Map(document.getElementById("map"), {
