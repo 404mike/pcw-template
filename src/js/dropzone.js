2c073d9bb046e7fbda7517802289f181036c80f5
Mike Jones <michaeljones401@gmail.com>
2024-09-03 08:20:58 +0100

    updating dropzone logic

--- src/js/dropzone.js
+++ src/js/dropzone.js
@@ -6,34 +6,41 @@ let numFiles = 0;
 
 const initDropzone = () => {
 
-    if(document.getElementById("dropzone") === null) {
+    if (document.getElementById("dropzone") === null) {
         return;
     }
 
     let myDropzone = new Dropzone("div#dropzone", { 
-        url: "/file/post",
+        url: "/file/post",  // This will not actually work without a server
+        autoProcessQueue: false, // Prevent auto processing when file is dropped
         addRemoveLinks: true,
         acceptedMimeTypes: "image/png,image/jpeg,audio/mp4,video/mp4,audio/mp3,audio/mpeg,application/pdf",
         dictDefaultMessage: "",
+        init: function() {
+            this.on("addedfile", (file) => {
+                // Fake a successful upload with a timeout
+                setTimeout(() => {
+                    this.emit("success", file);
+                    this.emit("complete", file);
+                }, 500);
+            });
+        }
     });
 
-    myDropzone.on("addedfile", (file) => {});
     myDropzone.on("success", (file) => {});
 
     myDropzone.on("removedfile", (file) => {
-        console.log("removedfile");
         numFiles--;
+        checkNumberOfUploadedFiles();
+    });
+
+    myDropzone.on("complete", (file) => {
+        numFiles++;
         checkNumberOfUploadedFiles();
     });
 
     myDropzone.on("error", (file, errorMessage) => {
         console.log("error");
-    });
-
-    myDropzone.on("complete", (file) => {
-        console.log("complete");
-        numFiles++;
-        checkNumberOfUploadedFiles();
     });
 };
 
@@ -45,31 +52,18 @@ const checkNumberOfUploadedFiles = () => {
     const multipartFileElement = document.getElementById("dropzone-multipart-file");
     const externalResource = document.getElementById("external-resource-upload-form");
 
-    if (numFiles === 1) {
-        singleFileElement.classList.remove("d-none");
-        singleFileElement.classList.add("d-block");
-        multipartFileElement.classList.remove("d-block");
-        multipartFileElement.classList.add("d-none");
-        externalResource.classList.remove("d-block");
-        externalResource.classList.add("d-none");
-    } 
-    if(numFiles > 1) {
-        singleFileElement.classList.remove("d-block");
-        singleFileElement.classList.add("d-none");
-        multipartFileElement.classList.remove("d-none");
-        multipartFileElement.classList.add("d-block");
-        externalResource.classList.remove("d-block");
-        externalResource.classList.add("d-none");
-    }
-    if(numFiles === 0) {
-        singleFileElement.classList.remove("d-block");
-        singleFileElement.classList.add("d-none");
-        multipartFileElement.classList.remove("d-block");
-        multipartFileElement.classList.add("d-none");
-        externalResource.classList.remove("d-none");
-        externalResource.classList.add("d-block");
-    }
+    numFiles === 1 ? toggleVisibility(singleFileElement, multipartFileElement, externalResource, true) :
+    numFiles > 1 ? toggleVisibility(singleFileElement, multipartFileElement, externalResource, false, true) :
+    toggleVisibility(singleFileElement, multipartFileElement, externalResource, false, false, true);
 };
 
+const toggleVisibility = (single, multiple, external, showSingle = false, showMultiple = false, showExternal = false) => {
+    single.classList.toggle("d-block", showSingle);
+    single.classList.toggle("d-none", !showSingle);
+    multiple.classList.toggle("d-block", showMultiple);
+    multiple.classList.toggle("d-none", !showMultiple);
+    external.classList.toggle("d-block", showExternal);
+    external.classList.toggle("d-none", !showExternal);
+};
 
 export { initDropzone };
